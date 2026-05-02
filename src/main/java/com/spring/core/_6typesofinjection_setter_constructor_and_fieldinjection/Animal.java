ae0490704f6174033a387c6849f495555626598a
Saurabh <saurabhthakur869@gmail.com>
2026-05-02 14:42:40 +0530

    only tight copule

--- src/main/java/com/spring/core/_6typesofinjection_setter_constructor_and_fieldinjection/Animal.java
+++
@@ -1,6 +0,0 @@
-package com.spring.core._6typesofinjection_setter_constructor_and_fieldinjection;
-
-public interface Animal {
-
-    public void play();
-}
--- src/main/java/com/spring/core/_7Anotherway_creatingbean_bean_annotation/Animal.java
+++
@@ -1,7 +0,0 @@
-package com.spring.core._7Anotherway_creatingbean_bean_annotation;
-
-
-public interface Animal {
-
-    public void play();
-}
