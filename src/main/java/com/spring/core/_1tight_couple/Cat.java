e77fde1a5483dbec60df33503b2cb6c581da761e
Saurabh <saurabhthakur869@gmail.com>
2026-05-02 21:16:16 +0530

    Add this

---
+++ src/main/java/com/spring/core/_1tight_couple/Cat.java
@@ -0,0 +1,14 @@
+//package com.spring.core.TightCouple;
+//
+//public class Cat extends Animal {
+//
+//    public Cat(){
+//        System.out.println("Creating Cat");
+//    }
+//
+//    @Override
+//    public void play(){
+//        super.play();
+//        System.out.println("Cat is playing");
+//    }
+//}
---
+++ src/main/java/com/spring/core/_1tight_couple/Persion.java
@@ -0,0 +1,11 @@
+package com.spring.core._1tight_couple;
+
+public class Persion {
+
+    Animal animal=new Animal();
+
+    public void playWithAnimal(){
+        //Using animal
+        animal.play();
+    }
+}
